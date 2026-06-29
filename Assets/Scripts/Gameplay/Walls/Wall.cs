fd48a8ae050e9b3032beed028a1c8dd29e9e9b1b
AlexNK001 <alexandrnovickij92@gmail.com>
2026-06-29 22:04:17 +0300

    fix

--- Assets/Scripts/Gameplay/Walls/Wall.cs
+++ Assets/Scripts/Gameplay/Walls/Wall.cs
@@ -3,6 +3,7 @@
 public class Wall : MonoBehaviour
 {
     private const float Width = 1f;
+    private const float Half = 0.5f;
     
     [SerializeField] private BoxCollider2D _upperCollider;
     [SerializeField] private BoxCollider2D _lowerCollider;
@@ -10,14 +11,13 @@ public class Wall : MonoBehaviour
 
     public void SetColliderHeight(int upperHeight, int scoringHeight, int lowerHeight)
     {
-        _upperCollider.transform.localPosition = new Vector2(0f, upperHeight * 0.5f);
+        _upperCollider.transform.localPosition = new Vector2(0f, upperHeight * Half);
         _upperCollider.transform.localScale = new Vector2(Width, upperHeight);
 
-        _scoringArea.transform.localPosition = new Vector2(0f, upperHeight + scoringHeight * 0.5f);
+        _scoringArea.transform.localPosition = new Vector2(0f, upperHeight + scoringHeight * Half);
         _scoringArea.transform.localScale = new Vector2(Width, scoringHeight);
 
-        _lowerCollider.transform.localPosition = new Vector2(0f, upperHeight + scoringHeight + lowerHeight * 0.5f);
+        _lowerCollider.transform.localPosition = new Vector2(0f, upperHeight + scoringHeight + lowerHeight * Half);
         _lowerCollider.transform.localScale = new Vector2(Width, lowerHeight);
     }
 }
-
